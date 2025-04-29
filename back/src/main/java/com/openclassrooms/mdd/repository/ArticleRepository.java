f273f18f3b162205b014f567c8739c40fc6887e7
ilyas cherfi <ilyascherfi91@gmail.com>
2025-04-29 11:40:55 +0200

    fixe name method in Article repository

--- back/src/main/java/com/openclassrooms/mdd/repository/ArticleRepository.java
+++ back/src/main/java/com/openclassrooms/mdd/repository/ArticleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
-  public List<Article> findByThemeThemeId(Integer themeId);
+  public List<Article> findByThemeId(Integer themeId);
 }
