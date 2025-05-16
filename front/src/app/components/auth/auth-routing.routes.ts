baba3ae7c81cc3d93f120c5542c4300e3a19d074
ilyas cherfi <ilyascherfi91@gmail.com>
2025-05-16 09:10:40 +0200

    some refacto

--- front/src/app/components/auth/auth-routing.routes.ts
+++ front/src/app/components/auth/auth-routing.routes.ts
@@ -3,7 +3,7 @@ import { RegisterComponent } from "./register/register.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 export const routes: Routes = [
-    { title: 'Home', path: '', component: HomeComponent },
-    { title: 'Register', path: 'register', component: RegisterComponent },
-    { title: 'Login', path: 'login', component: LoginComponent }
+  { title: 'Home', path: '', component: HomeComponent },
+  { title: 'Register', path: 'register', component: RegisterComponent },
+  { title: 'Login', path: 'login', component: LoginComponent }
 ];
