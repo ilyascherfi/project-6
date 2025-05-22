3014b5f51a9a7b3b1a5af9feebc4c3a4af8496ee
ilyas cherfi <ilyascherfi91@gmail.com>
2025-05-22 18:13:13 +0200

    and guard to app routes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { ArticlesComponent } from "./components/articles/articles.component";
+import { AuthGuard } from './guards/auth.guard';
+import { UnauthGuard } from './guards/unauth.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
+    canActivate: [UnauthGuard],
     loadChildren: () => import('./components/auth/auth-routing.routes').then(m => m.routes)
   },
-  { title: 'articles', path: 'articles', component: ArticlesComponent }
+  { path: '', redirectTo: '/articles', pathMatch: 'full' },
+  {
+    path: 'articles',
+    canActivate: [AuthGuard],
+    component: ArticlesComponent
+  },
 ];
