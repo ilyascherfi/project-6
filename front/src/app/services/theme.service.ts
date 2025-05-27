c21542ad0f3d35482738b296ee9dd8fb152e8688
ilyas cherfi <ilyascherfi91@gmail.com>
2025-05-27 13:49:26 +0200

    put declaration before the constructor

--- front/src/app/services/theme.service.ts
+++ front/src/app/services/theme.service.ts
@@ -10,28 +10,29 @@ export class ThemeService implements OnDestroy {
 
   private pathService = 'http://localhost:8080/api/theme';
 
-  constructor(private httpClient: HttpClient) {
-      this.subscription = this.loadInitialData().pipe(take(1)).subscribe();
-  }
   public subscription!: Subscription
   public themes: Theme[] = {} as Theme[];
   public themes$ = new BehaviorSubject<Theme[]>(this.themes)
 
+  constructor(private httpClient: HttpClient) {
+    this.subscription = this.loadInitialData().pipe(take(1)).subscribe();
+  }
+
   ngOnDestroy(): void {
-      this.subscription.unsubscribe();
+    this.subscription.unsubscribe();
   }
 
   public loadInitialData() {
-      return this.httpClient.get<Theme[]>(`${this.pathService}`).pipe(
-          tap((value) => this.themes$.next(value)),
-          catchError((error, caught) => {
-              console.error("An error occured loading data");
-              this.themes$.next(this.themes);
-              return caught;
-          })
-      );
+    return this.httpClient.get<Theme[]>(`${this.pathService}`).pipe(
+      tap((value) => this.themes$.next(value)),
+      catchError((error, caught) => {
+        console.error("An error occured loading data");
+        this.themes$.next(this.themes);
+        return caught;
+      })
+    );
   }
   getThemes() {
-      return this.themes$.asObservable();
+    return this.themes$.asObservable();
   }
 }
