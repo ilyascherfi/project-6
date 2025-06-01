1a5f9490c1400d0fd05f57bbdcfcaaa8822cb9d1
ilyas cherfi <ilyascherfi91@gmail.com>
2025-06-01 19:37:30 +0200

    fixe mapping error

--- back/src/main/java/com/openclassrooms/mdd/configuration/ModelMapperConfig.java
+++ back/src/main/java/com/openclassrooms/mdd/configuration/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package com.openclassrooms.mdd.configuration;
 
+import com.openclassrooms.mdd.model.Article;
+import com.openclassrooms.mdd.model.dto.ReturnArticleDTO;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,22 @@ public class ModelMapperConfig {
 
   @Bean
   public ModelMapper modelMapper() {
-    return new ModelMapper();
+    ModelMapper modelMapper = new ModelMapper();
+
+    // Configuration personnalisée pour le mapping Article -> ReturnArticleDTO
+    modelMapper.addMappings(new PropertyMap<Article, ReturnArticleDTO>() {
+      @Override
+      protected void configure() {
+        map().setArticleId(Long.valueOf(source.getId()));
+        map().setTitle(source.getTitle());
+        map().setAuthor(source.getUser().getId());
+        map().setTheme(source.getTheme().getId());
+        map().setDate(source.getDate());
+        map().setContent(source.getContent());
+      }
+    });
+
+    return modelMapper;
   }
 }
+
