bac8fef1e0ae74d9dbcfe773967d870b0f49ce1f
ilyas cherfi <ilyascherfi91@gmail.com>
2025-06-03 10:44:00 +0200

    handle unsubscribe from profile

--- front/src/app/components/theme/theme.component.ts
+++ front/src/app/components/theme/theme.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ThemeProps } from '../../interfaces/theme-props.class';
 import { SessionService } from '../../services/session.service';
@@ -6,37 +6,37 @@ import { UserService } from '../../services/user-service.service';
 
 @Component({
   selector: 'app-theme',
-  imports: [],
   templateUrl: './theme.component.html',
-  styleUrl: './theme.component.scss'
+  styleUrls: ['./theme.component.scss']
 })
 export class ThemeComponent implements OnDestroy {
 
-  themeProps = input.required<ThemeProps>();
+  @Input() themeProps!: ThemeProps;
+  @Input() articleView: boolean = false;
 
   public onError: boolean = false;
   public subscription: Subscription | undefined;
 
-
   constructor(
     private sessionService: SessionService,
     private userService: UserService,
-  ) { };
+  ) { }
 
   ngOnDestroy(): void {
     this.unSubscribeObservableIfSubscription();
   }
+
   public unSubscribeObservableIfSubscription(): void {
-    if (this.subscription != undefined) {
-      this.subscription.unsubscribe()
+    if (this.subscription !== undefined) {
+      this.subscription.unsubscribe();
     }
   }
 
   public onSubscribe(): void {
     this.unSubscribeObservableIfSubscription();
-    this.subscription = this.userService.themeSubscribe(this.themeProps().theme.themeId).subscribe({
+    this.subscription = this.userService.themeSubscribe(this.themeProps.theme.themeId).subscribe({
       next: () => {
-        this.sessionService.addTheme(this.themeProps().theme);
+        this.sessionService.addTheme(this.themeProps.theme);
       },
       error: (error: any) => {
         this.onError = true;
@@ -45,5 +45,16 @@ export class ThemeComponent implements OnDestroy {
     });
   }
 
-
+  public onUnsubscribe(): void {
+    this.unSubscribeObservableIfSubscription();
+    this.subscription = this.userService.themeUnsubscribe(this.themeProps.theme.themeId).subscribe({
+      next: () => {
+        this.sessionService.removeTheme(this.themeProps.theme);
+      },
+      error: (error: any) => {
+        this.onError = true;
+        console.log(error);
+      },
+    });
+  }
 }
